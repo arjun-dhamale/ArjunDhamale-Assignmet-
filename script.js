5699c3e80bc330862e1cf7bb6b182be191a3c04e
Arjun Dhamale <146521338+arjun-dhamale@users.noreply.github.com>
2025-01-22 18:58:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const activateBtn = document.querySelector(".activate-btn");
+// Add a 'click' event listener to the button
+activateBtn.addEventListener("click", () => {
+    alert("Activated Successfully!");
+});
